293759fe76a1eb18ece3c5aa27a5a95bbbad7450
Alright <mzlot555@gmail.com>
2024-08-27 15:14:03 -0400

    fix H256 impl

--- src/hash.rs
+++ src/hash.rs
@@ -1,12 +1,17 @@
+//! Fixed-size hashes
 use rustc_hex::{FromHex, FromHexError, ToHex};
 use std::hash::{Hash, Hasher};
 use std::{cmp, fmt, ops, str};
+use serde;
+use serde::de::Unexpected;
+use std::cmp::Ordering;
+use std::str::FromStr;
 
-macro_rules! impl_hash {
+macro_rules! impl_global_hash {
     ($name: ident, $size: expr) => {
         #[derive(Copy)]
         #[repr(C)]
-        pub struct $name(pub [u8; $size]);
+        pub struct $name([u8; $size]);
 
         impl Default for $name {
             fn default() -> Self { $name([0u8; $size]) }
@@ -124,13 +129,116 @@ macro_rules! impl_hash {
             }
         }
 
+        impl Eq for $name {}
+
+        impl $name {
+            pub fn take(self) -> [u8; $size] { self.0 }
+
+            pub fn as_slice(&self) -> &[u8] { &self.0 }
+
+            pub fn reversed(&self) -> Self {
+                let mut result = self.clone();
+                result.reverse();
+                result
+            }
+
+            pub fn size() -> usize { $size }
+
+            pub fn is_zero(&self) -> bool { self.0.iter().all(|b| *b == 0) }
+        }
+    };
+}
+
+macro_rules! impl_hash {
+    ($name: ident, $other: ident, $size: expr) => {
+        /// Hash serialization
+        #[derive(Clone, Copy)]
+        pub struct $name(pub [u8; $size]);
+
+        impl $name {
+            pub const fn const_default() -> $name { $name([0; $size]) }
+        }
+
+        impl Default for $name {
+            fn default() -> Self { $name::const_default() }
+        }
+
+        impl fmt::Display for $name {
+            fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> { write!(f, "{:02x}", self) }
+        }
+
+        impl fmt::Debug for $name {
+            fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> { write!(f, "{:02x}", self) }
+        }
+
+        impl<T> From<T> for $name
+        where
+            $other: From<T>,
+        {
+            fn from(o: T) -> Self { $name($other::from(o).take()) }
+        }
+
+        impl FromStr for $name {
+            type Err = <$other as FromStr>::Err;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                let other = $other::from_str(s)?;
+                Ok($name(other.take()))
+            }
+        }
+
+        #[allow(clippy::from_over_into)]
+        impl Into<$other> for $name {
+            fn into(self) -> $other { $other::from(self.0) }
+        }
+
+        #[allow(clippy::from_over_into)]
+        impl Into<Vec<u8>> for $name {
+            fn into(self) -> Vec<u8> { self.0.to_vec() }
+        }
+
+        impl Eq for $name {}
+
+        impl Ord for $name {
+            fn cmp(&self, other: &Self) -> Ordering {
+                let self_ref: &[u8] = &self.0;
+                let other_ref: &[u8] = &other.0;
+                self_ref.cmp(other_ref)
+            }
+        }
+
+        impl PartialEq for $name {
+            fn eq(&self, other: &Self) -> bool {
+                let self_ref: &[u8] = &self.0;
+                let other_ref: &[u8] = &other.0;
+                self_ref == other_ref
+            }
+        }
+
+        impl PartialOrd for $name {
+            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                let self_ref: &[u8] = &self.0;
+                let other_ref: &[u8] = &other.0;
+                self_ref.partial_cmp(other_ref)
+            }
+        }
+
+        impl Hash for $name {
+            fn hash<H>(&self, state: &mut H)
+            where
+                H: Hasher,
+            {
+                $other::from(self.0.clone()).hash(state)
+            }
+        }
+
         impl serde::Serialize for $name {
             fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
             where
                 S: serde::Serializer,
             {
                 let mut hex = String::new();
-                hex.push_str(&self.0.clone().to_hex::<String>());
+                hex.push_str(&$other::from(self.0.clone()).to_hex::<String>());
                 serializer.serialize_str(&hex)
             }
         }
@@ -154,16 +262,16 @@ macro_rules! impl_hash {
                         E: serde::de::Error,
                     {
                         if value.len() != $size * 2 {
-                            return Err(E::invalid_value(serde::de::Unexpected::Str(value), &self));
+                            return Err(E::invalid_value(Unexpected::Str(value), &self));
                         }
 
                         match value[..].from_hex::<Vec<u8>>() {
                             Ok(ref v) => {
                                 let mut result = [0u8; $size];
                                 result.copy_from_slice(v);
-                                Ok($name(result))
+                                Ok($name($other::from(result).take()))
                             },
-                            _ => Err(E::invalid_value(serde::de::Unexpected::Str(value), &self)),
+                            _ => Err(E::invalid_value(Unexpected::Str(value), &self)),
                         }
                     }
 
@@ -187,33 +295,63 @@ macro_rules! impl_hash {
                 Ok(())
             }
         }
-
-        impl Eq for $name {}
-
-        impl $name {
-            pub fn take(self) -> [u8; $size] { self.0 }
-
-            pub fn as_slice(&self) -> &[u8] { &self.0 }
-
-            pub fn reversed(&self) -> Self {
-                let mut result = self.clone();
-                result.reverse();
-                result
-            }
-
-            pub fn size() -> usize { $size }
-
-            pub fn is_zero(&self) -> bool { self.0.iter().all(|b| *b == 0) }
-        }
     };
 }
 
-impl_hash!(H256, 32);
-
 impl H256 {
     #[inline]
-    pub fn from_reversed_str(s: &'static str) -> Self { H256::from(s).reversed() }
+    pub fn reversed(&self) -> Self {
+        let mut result = *self;
+        result.0.reverse();
+        result
+    }
+}
+
+impl GlobalH256 {
+    #[inline]
+    pub fn from_reversed_str(s: &'static str) -> Self { GlobalH256::from(s).reversed() }
 
     #[inline]
     pub fn to_reversed_str(self) -> String { self.reversed().to_string() }
+}
+
+impl_global_hash!(GlobalH256, 32);
+impl_hash!(H256, GlobalH256, 32);
+
+#[cfg(test)]
+mod tests {
+    use super::H256;
+    use primitives::hash::H256 as GlobalH256;
+    use std::str::FromStr;
+
+    #[test]
+    fn hash_debug() {
+        let str_reversed = "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048";
+        let reversed_hash = H256::from(str_reversed);
+        let debug_result = format!("{:?}", reversed_hash);
+        assert_eq!(debug_result, str_reversed);
+    }
+
+    #[test]
+    fn hash_from_str() {
+        let str_reversed = "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048";
+        match H256::from_str(str_reversed) {
+            Ok(reversed_hash) => assert_eq!(format!("{:?}", reversed_hash), str_reversed),
+            _ => panic!("unexpected"),
+        }
+
+        let str_reversed = "XXXYYY";
+        if H256::from_str(str_reversed).is_ok() {
+            panic!("unexpected");
+        }
+    }
+
+    #[test]
+    fn hash_to_global_hash() {
+        let str_reversed = "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048";
+        let reversed_hash = H256::from(str_reversed);
+        let global_hash = GlobalH256::from(str_reversed);
+        let global_converted: GlobalH256 = reversed_hash.into();
+        assert_eq!(global_converted, global_hash);
+    }
 }
