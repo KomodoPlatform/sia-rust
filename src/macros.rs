29ae68d0f843e2012d8f4e8e961ef2fa0349d17f
Alright <mzlot555@gmail.com>
2024-09-21 00:54:20 -0400

    change cross_target_test macro to process multiple unit tests

--- src/macros.rs
+++ src/macros.rs
@@ -1,15 +1,21 @@
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen_test::wasm_bindgen_test;
+// Run a block of unit tests on both wasm32 and non-wasm32 targets
+#[cfg(test)]
+macro_rules! cross_target_tests {
+    ($($test_fn:item)*) => {
+        #[cfg(all(test, target_arch = "wasm32"))]
+        use wasm_bindgen_test::*;
 
-// Run a unit test on both wasm32 and non-wasm32 targets
-macro_rules! cross_target_test {
-    ($test:item) => {
-        #[cfg(target_arch = "wasm32")]
-        #[wasm_bindgen_test]
-        $test
+        #[cfg(all(test, target_arch = "wasm32"))]
+        wasm_bindgen_test_configure!(run_in_browser);
 
-        #[cfg(not(target_arch = "wasm32"))]
-        #[test]
-        $test
+        $(
+            #[cfg(all(test, target_arch = "wasm32"))]
+            #[wasm_bindgen_test]
+            $test_fn
+
+            #[cfg(not(target_arch = "wasm32"))]
+            #[test]
+            $test_fn
+        )*
     };
 }
