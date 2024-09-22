530c7d0c39ddd7cb1481dc805818ae46a8eafee3
Alright <mzlot555@gmail.com>
2024-09-22 12:05:09 -0400

    wrap tests/spend_policy in cross_target_tests

--- src/tests/spend_policy.rs
+++ src/tests/spend_policy.rs
@@ -1,164 +1,164 @@
-use crate::types::{Address, Hash256, PublicKey, spend_policy_atomic_swap_success, SpendPolicy, UnlockCondition, UnlockKey};
-use std::convert::TryFrom;
-use std::str::FromStr;
 
-#[test]
-fn test_serde_spend_policy_above() {
-    let j = json!(
-      {
-        "type": "above",
-        "policy": 100
-      }
-    );
 
-    let spend_policy_deser = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
-    let spend_policy = SpendPolicy::Above(100);
+#[cfg(test)]
+mod test {
+    use crate::types::{Address, Hash256, PublicKey, spend_policy_atomic_swap_success, SpendPolicy, UnlockCondition, UnlockKey};
+    use std::convert::TryFrom;
+    use std::str::FromStr;
 
-    assert_eq!(spend_policy, spend_policy_deser);
-}
+    cross_target_tests! {
+        fn test_serde_spend_policy_above() {
+            let j = json!(
+            {
+                "type": "above",
+                "policy": 100
+            }
+            );
 
-#[test]
-fn test_serde_spend_policy_after() {
-    let j = json!(
-      {
-        "type": "after",
-        "policy": 200
-      }
-    );
+            let spend_policy_deser = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
+            let spend_policy = SpendPolicy::Above(100);
 
-    let spend_policy_deser = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
-    let spend_policy = SpendPolicy::After(200);
+            assert_eq!(spend_policy, spend_policy_deser);
+        }
 
-    assert_eq!(spend_policy, spend_policy_deser);
-}
+        fn test_serde_spend_policy_after() {
+            let j = json!(
+            {
+                "type": "after",
+                "policy": 200
+            }
+            );
 
-#[test]
-fn test_serde_spend_policy_public_key() {
-    let j = json!(
-      {
-        "type": "pk",
-        "policy": "ed25519:0102030000000000000000000000000000000000000000000000000000000000"
-      }
-    );
-    let pubkey = PublicKey::from_bytes(
-        &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
-    )
-    .unwrap();
-    let spend_policy_deser: SpendPolicy = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
-    let spend_policy = SpendPolicy::PublicKey(pubkey);
+            let spend_policy_deser = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
+            let spend_policy = SpendPolicy::After(200);
 
-    assert_eq!(spend_policy, spend_policy_deser);
-}
+            assert_eq!(spend_policy, spend_policy_deser);
+        }
 
-#[test]
-fn test_serde_spend_policy_hash() {
-    let j = json!(
-      {
-        "type": "h",
-        "policy": "h:0102030000000000000000000000000000000000000000000000000000000000"
-    }
-    );
-    let hash = Hash256::try_from("h:0102030000000000000000000000000000000000000000000000000000000000").unwrap();
-    let spend_policy_deser: SpendPolicy = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
-    let spend_policy = SpendPolicy::Hash(hash);
+        fn test_serde_spend_policy_public_key() {
+            let j = json!(
+            {
+                "type": "pk",
+                "policy": "ed25519:0102030000000000000000000000000000000000000000000000000000000000"
+            }
+            );
+            let pubkey = PublicKey::from_bytes(
+                &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
+            )
+            .unwrap();
+            let spend_policy_deser: SpendPolicy = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
+            let spend_policy = SpendPolicy::PublicKey(pubkey);
 
-    assert_eq!(spend_policy, spend_policy_deser);
-}
+            assert_eq!(spend_policy, spend_policy_deser);
+        }
 
-#[test]
-fn test_serde_spend_policy_opaque() {
-    let j = json!(
-      {
-        "type": "opaque",
-        "policy": "addr:f72e84ee9e344e424a6764068ffd7fdce4b4e50609892c6801bc1ead79d3ae0d71791b277a3a"
-    }
-    );
-    let address =
-        Address::from_str("addr:f72e84ee9e344e424a6764068ffd7fdce4b4e50609892c6801bc1ead79d3ae0d71791b277a3a").unwrap();
-    let spend_policy_deser: SpendPolicy = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
-    let spend_policy = SpendPolicy::Opaque(address);
+        fn test_serde_spend_policy_hash() {
+            let j = json!(
+            {
+                "type": "h",
+                "policy": "h:0102030000000000000000000000000000000000000000000000000000000000"
+            }
+            );
+            let hash = Hash256::try_from("h:0102030000000000000000000000000000000000000000000000000000000000").unwrap();
+            let spend_policy_deser: SpendPolicy = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
+            let spend_policy = SpendPolicy::Hash(hash);
 
-    assert_eq!(spend_policy, spend_policy_deser);
-}
+            assert_eq!(spend_policy, spend_policy_deser);
+        }
 
-#[test]
-fn test_serde_spend_policy_threshold() {
-    let alice_pubkey = PublicKey::from_bytes(
-        &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
-    )
-    .unwrap();
-    let bob_pubkey = PublicKey::from_bytes(
-        &hex::decode("06C87838297B7BB16AB23946C99DFDF77FF834E35DB07D71E9B1D2B01A11E96D").unwrap(),
-    )
-    .unwrap();
+        fn test_serde_spend_policy_opaque() {
+            let j = json!(
+            {
+                "type": "opaque",
+                "policy": "addr:f72e84ee9e344e424a6764068ffd7fdce4b4e50609892c6801bc1ead79d3ae0d71791b277a3a"
+            }
+            );
+            let address =
+                Address::from_str("addr:f72e84ee9e344e424a6764068ffd7fdce4b4e50609892c6801bc1ead79d3ae0d71791b277a3a").unwrap();
+            let spend_policy_deser: SpendPolicy = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
+            let spend_policy = SpendPolicy::Opaque(address);
 
-    let secret_hash = Hash256::try_from("h:0100000000000000000000000000000000000000000000000000000000000000").unwrap();
-    let spend_policy = spend_policy_atomic_swap_success(alice_pubkey, bob_pubkey, 77777777, secret_hash);
+            assert_eq!(spend_policy, spend_policy_deser);
+        }
 
-    let j = json!(
-        {
-            "type": "thresh",
-            "policy": {
-                "n": 1,
-                "of": [
-                    {
-                        "type": "thresh",
-                        "policy": {
-                            "n": 2,
-                            "of": [
-                                {
-                                    "type": "pk",
-                                    "policy": "ed25519:0102030000000000000000000000000000000000000000000000000000000000"
-                                },
-                                {
-                                    "type": "h",
-                                    "policy": "h:0100000000000000000000000000000000000000000000000000000000000000"
+        fn test_serde_spend_policy_threshold() {
+            let alice_pubkey = PublicKey::from_bytes(
+                &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
+            )
+            .unwrap();
+            let bob_pubkey = PublicKey::from_bytes(
+                &hex::decode("06C87838297B7BB16AB23946C99DFDF77FF834E35DB07D71E9B1D2B01A11E96D").unwrap(),
+            )
+            .unwrap();
+
+            let secret_hash = Hash256::try_from("h:0100000000000000000000000000000000000000000000000000000000000000").unwrap();
+            let spend_policy = spend_policy_atomic_swap_success(alice_pubkey, bob_pubkey, 77777777, secret_hash);
+
+            let j = json!(
+                {
+                    "type": "thresh",
+                    "policy": {
+                        "n": 1,
+                        "of": [
+                            {
+                                "type": "thresh",
+                                "policy": {
+                                    "n": 2,
+                                    "of": [
+                                        {
+                                            "type": "pk",
+                                            "policy": "ed25519:0102030000000000000000000000000000000000000000000000000000000000"
+                                        },
+                                        {
+                                            "type": "h",
+                                            "policy": "h:0100000000000000000000000000000000000000000000000000000000000000"
+                                        }
+                                    ]
                                 }
-                            ]
-                        }
-                    },
-                    {
-                        "type": "opaque",
-                        "policy": "addr:f72e84ee9e344e424a6764068ffd7fdce4b4e50609892c6801bc1ead79d3ae0d71791b277a3a"
+                            },
+                            {
+                                "type": "opaque",
+                                "policy": "addr:f72e84ee9e344e424a6764068ffd7fdce4b4e50609892c6801bc1ead79d3ae0d71791b277a3a"
+                            }
+                        ]
                     }
-                ]
-            }
+                }
+            );
+
+            let spend_policy_deser: SpendPolicy = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
+
+            assert_eq!(spend_policy, spend_policy_deser);
         }
-    );
 
-    let spend_policy_deser: SpendPolicy = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
+        fn test_serde_spend_policy_unlock_conditions_standard() {
+            let j = json!(
+                {
+                    "type": "uc",
+                    "policy": {
+                        "timelock": 0,
+                        "publicKeys": [
+                            "ed25519:0102030000000000000000000000000000000000000000000000000000000000"
+                        ],
+                        "signaturesRequired": 1
+                    }
+                }
+            );
 
-    assert_eq!(spend_policy, spend_policy_deser);
-}
+            let public_key = PublicKey::from_bytes(
+                &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
+            )
+            .unwrap();
 
-#[test]
-fn test_serde_spend_policy_unlock_conditions_standard() {
-    let j = json!(
-        {
-            "type": "uc",
-            "policy": {
-                "timelock": 0,
-                "publicKeys": [
-                    "ed25519:0102030000000000000000000000000000000000000000000000000000000000"
-                ],
-                "signaturesRequired": 1
-            }
+            let uc = UnlockCondition {
+                unlock_keys: vec![UnlockKey::Ed25519(public_key)],
+                timelock: 0,
+                signatures_required: 1,
+            };
+
+            let spend_policy_deser: SpendPolicy = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
+            let spend_policy = SpendPolicy::UnlockConditions(uc);
+
+            assert_eq!(spend_policy, spend_policy_deser);
         }
-    );
-
-    let public_key = PublicKey::from_bytes(
-        &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
-    )
-    .unwrap();
-
-    let uc = UnlockCondition {
-        unlock_keys: vec![UnlockKey::Ed25519(public_key)],
-        timelock: 0,
-        signatures_required: 1,
-    };
-
-    let spend_policy_deser: SpendPolicy = serde_json::from_value::<SpendPolicy>(j).unwrap().into();
-    let spend_policy = SpendPolicy::UnlockConditions(uc);
-
-    assert_eq!(spend_policy, spend_policy_deser);
+    }
 }
