8afa970677775a370b879499cd0efc4f3067eb38
Alright <mzlot555@gmail.com>
2024-09-22 04:26:20 -0400

    wrap tests in mod

--- src/encoding.rs
+++ src/encoding.rs
@@ -115,90 +115,96 @@ impl Encodable for Hash256 {
     fn encode(&self, encoder: &mut Encoder) { encoder.write_slice(&self.0); }
 }
 
-#[test]
-fn test_encoder_default_hash() {
-    assert_eq!(
-        Encoder::default().hash(),
-        Hash256::try_from("h:0e5751c026e543b2e8ab2eb06099daa1d1e5df47778f7787faab45cdf12fe3a8").unwrap()
-    )
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::convert::TryFrom;
 
-#[test]
-fn test_encoder_write_bytes() {
-    let mut encoder = Encoder::default();
-    encoder.write_len_prefixed_bytes(&[1, 2, 3, 4]);
-    assert_eq!(
-        encoder.hash(),
-        Hash256::try_from("h:d4a72b52e2e1f40e20ee40ea6d5080a1b1f76164786defbb7691a4427f3388f5").unwrap()
-    );
-}
+    #[test]
+    fn test_encoder_default_hash() {
+        assert_eq!(
+            Encoder::default().hash(),
+            Hash256::try_from("h:0e5751c026e543b2e8ab2eb06099daa1d1e5df47778f7787faab45cdf12fe3a8").unwrap()
+        )
+    }
 
-#[test]
-fn test_encoder_write_u8() {
-    let mut encoder = Encoder::default();
-    encoder.write_u8(1);
-    assert_eq!(
-        encoder.hash(),
-        Hash256::try_from("h:ee155ace9c40292074cb6aff8c9ccdd273c81648ff1149ef36bcea6ebb8a3e25").unwrap()
-    );
-}
+    #[test]
+    fn test_encoder_write_bytes() {
+        let mut encoder = Encoder::default();
+        encoder.write_len_prefixed_bytes(&[1, 2, 3, 4]);
+        assert_eq!(
+            encoder.hash(),
+            Hash256::try_from("h:d4a72b52e2e1f40e20ee40ea6d5080a1b1f76164786defbb7691a4427f3388f5").unwrap()
+        );
+    }
 
-#[test]
-fn test_encoder_write_u64() {
-    let mut encoder = Encoder::default();
-    encoder.write_u64(1);
-    assert_eq!(
-        encoder.hash(),
-        Hash256::try_from("h:1dbd7d0b561a41d23c2a469ad42fbd70d5438bae826f6fd607413190c37c363b").unwrap()
-    );
-}
+    #[test]
+    fn test_encoder_write_u8() {
+        let mut encoder = Encoder::default();
+        encoder.write_u8(1);
+        assert_eq!(
+            encoder.hash(),
+            Hash256::try_from("h:ee155ace9c40292074cb6aff8c9ccdd273c81648ff1149ef36bcea6ebb8a3e25").unwrap()
+        );
+    }
 
-#[test]
-fn test_encoder_write_distiguisher() {
-    let mut encoder = Encoder::default();
-    encoder.write_distinguisher("test");
-    assert_eq!(
-        encoder.hash(),
-        Hash256::try_from("h:25fb524721bf98a9a1233a53c40e7e198971b003bf23c24f59d547a1bb837f9c").unwrap()
-    );
-}
+    #[test]
+    fn test_encoder_write_u64() {
+        let mut encoder = Encoder::default();
+        encoder.write_u64(1);
+        assert_eq!(
+            encoder.hash(),
+            Hash256::try_from("h:1dbd7d0b561a41d23c2a469ad42fbd70d5438bae826f6fd607413190c37c363b").unwrap()
+        );
+    }
 
-#[test]
-fn test_encoder_write_bool() {
-    let mut encoder = Encoder::default();
-    encoder.write_bool(true);
-    assert_eq!(
-        encoder.hash(),
-        Hash256::try_from("h:ee155ace9c40292074cb6aff8c9ccdd273c81648ff1149ef36bcea6ebb8a3e25").unwrap()
-    );
-}
+    #[test]
+    fn test_encoder_write_distiguisher() {
+        let mut encoder = Encoder::default();
+        encoder.write_distinguisher("test");
+        assert_eq!(
+            encoder.hash(),
+            Hash256::try_from("h:25fb524721bf98a9a1233a53c40e7e198971b003bf23c24f59d547a1bb837f9c").unwrap()
+        );
+    }
 
-#[test]
-fn test_encoder_reset() {
-    let mut encoder = Encoder::default();
-    encoder.write_bool(true);
-    assert_eq!(
-        encoder.hash(),
-        Hash256::try_from("h:ee155ace9c40292074cb6aff8c9ccdd273c81648ff1149ef36bcea6ebb8a3e25").unwrap()
-    );
+    #[test]
+    fn test_encoder_write_bool() {
+        let mut encoder = Encoder::default();
+        encoder.write_bool(true);
+        assert_eq!(
+            encoder.hash(),
+            Hash256::try_from("h:ee155ace9c40292074cb6aff8c9ccdd273c81648ff1149ef36bcea6ebb8a3e25").unwrap()
+        );
+    }
 
-    encoder.reset();
-    encoder.write_bool(false);
-    assert_eq!(
-        encoder.hash(),
-        Hash256::try_from("h:03170a2e7597b7b7e3d84c05391d139a62b157e78786d8c082f29dcf4c111314").unwrap()
-    );
-}
+    #[test]
+    fn test_encoder_reset() {
+        let mut encoder = Encoder::default();
+        encoder.write_bool(true);
+        assert_eq!(
+            encoder.hash(),
+            Hash256::try_from("h:ee155ace9c40292074cb6aff8c9ccdd273c81648ff1149ef36bcea6ebb8a3e25").unwrap()
+        );
 
-#[test]
-fn test_encoder_complex() {
-    let mut encoder = Encoder::default();
-    encoder.write_distinguisher("test");
-    encoder.write_bool(true);
-    encoder.write_u8(1);
-    encoder.write_len_prefixed_bytes(&[1, 2, 3, 4]);
-    assert_eq!(
-        encoder.hash(),
-        Hash256::try_from("h:b66d7a9bef9fb303fe0e41f6b5c5af410303e428c4ff9231f6eb381248693221").unwrap()
-    );
+        encoder.reset();
+        encoder.write_bool(false);
+        assert_eq!(
+            encoder.hash(),
+            Hash256::try_from("h:03170a2e7597b7b7e3d84c05391d139a62b157e78786d8c082f29dcf4c111314").unwrap()
+        );
+    }
+
+    #[test]
+    fn test_encoder_complex() {
+        let mut encoder = Encoder::default();
+        encoder.write_distinguisher("test");
+        encoder.write_bool(true);
+        encoder.write_u8(1);
+        encoder.write_len_prefixed_bytes(&[1, 2, 3, 4]);
+        assert_eq!(
+            encoder.hash(),
+            Hash256::try_from("h:b66d7a9bef9fb303fe0e41f6b5c5af410303e428c4ff9231f6eb381248693221").unwrap()
+        );
+    }
 }
