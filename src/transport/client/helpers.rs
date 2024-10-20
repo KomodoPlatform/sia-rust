d7eb095e1955ac1355ad56fc064e76c8c1e90e4b
Alright <mzlot555@gmail.com>
2024-10-20 09:42:04 -0400

    add ApiClientHelpers::select_unspent_outputs

--- src/transport/client/helpers.rs
+++ src/transport/client/helpers.rs
@@ -2,8 +2,22 @@ use super::{ApiClient, ApiClientError};
 use crate::transport::endpoints::{AddressBalanceRequest, ConsensusTipRequest, GetAddressUtxosRequest, AddressBalanceResponse};
 use crate::types::{Address, Currency, SiacoinElement};
 use async_trait::async_trait;
+use thiserror::Error;
 
+#[derive(Debug, Error)]
+pub enum ApiClientHelpersError {
+    #[error("ApiClientHelpersError::SelectOutputs: insufficent amount, available: {:?} required: {:?}", .available, .required)]
+    SelectOutputs {
+        available: Currency,
+        required: Currency,
+    },
+    #[error("ApiClientHelpersError::ApiClientError: {0}")]
+    ApiClientError(#[from] ApiClientError),
+}
 
+/// Helper methods for the ApiClient trait
+/// These generally provide higher level functionality than the base ApiClient trait
+/// This crate is focused on catering to the Komodo Defi Framework integration
 #[async_trait]
 pub trait ApiClientHelpers: ApiClient {
     async fn current_height(&self) -> Result<u64, ApiClientError> {
@@ -18,8 +32,47 @@ pub trait ApiClientHelpers: ApiClient {
         self.dispatcher(GetAddressUtxosRequest { address: address.clone(), limit, offset }).await
     }
 
-    async fn select_utxos(&self, address: &Address, amount: Currency) -> Result<Vec<SiacoinElement>, ApiClientError> {
-        let _utxos = self.get_unspent_outputs(address, None, None).await?;
-        todo!()
+    /// Fetches unspent outputs for the given address and attemopts to select a subset of outputs
+    /// whose total value is at least `total_amount`. The outputs are sorted from largest to smallest to minimize
+    /// the number of outputs selected. The function returns a vector of the selected outputs and the difference between
+    /// the total value of the selected outputs and the required amount, aka the change.
+    /// # Arguments
+    ///
+    /// * `unspent_outputs` - A vector of `SiacoinElement`s representing unspent Siacoin outputs.
+    /// * `total_amount` - The total amount (in u128) required for the selection.
+    ///
+    /// # Returns
+    ///
+    /// This function returns `Result<(Vec<SiacoinElement>, Currency), ApiClientHelpersError>`:
+    /// * `Ok((Vec<SiacoinElement>, Currency))` - A tuple containing, a vector of the selected unspent outputs and the change amount.
+    /// * `Err(MmError<SelectOutputsError>)` - An error is returned if the available outputs cannot meet the required amount or a transport error is encountered.
+    async fn select_unspent_outputs(&self, address: &Address, total_amount: Currency) -> Result<(Vec<SiacoinElement>, Currency), ApiClientHelpersError> {
+        let mut unspent_outputs = self.get_unspent_outputs(address, None, None).await?;
+
+        // Sort outputs from largest to smallest
+        unspent_outputs.sort_by(|a, b| b.siacoin_output.value.0.cmp(&a.siacoin_output.value.0));
+
+        let mut selected = Vec::new();
+        let mut selected_amount = 0;
+
+        // Select outputs until the total amount is reached
+        for output in unspent_outputs {
+            selected_amount += *output.siacoin_output.value;
+            selected.push(output);
+
+            if selected_amount >= *total_amount {
+                break;
+            }
+        }
+
+        if selected_amount < *total_amount {
+            return Err(ApiClientHelpersError::SelectOutputs {
+                available: selected_amount.into(),
+                required: total_amount.into(),
+            });
+        }
+        let change = selected_amount as u128 - *total_amount;
+
+        Ok((selected, change.into()))
     }
 }
